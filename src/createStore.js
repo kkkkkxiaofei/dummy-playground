ab43bf78a592585cf4d71130b689a938285c8c91
Xiaofei ZHANG <xifzhang@thoughtworks.com>
2020-07-04 10:15:27 +0800

    add subscriber

--- src/createStore.js
+++ src/createStore.js
@@ -1,10 +1,21 @@
 export default reducer => {
-  let state = null;
-  const dispatch = action => reducer(action, state);
+  let state = null, listeners = [];
+  
   const getState = () => state;
+  const subscribe = listener => {
+    listeners.push(listener);
+    //unsubsribe
+    return () => listeners.splice(listeners.indexOf(listener), 1);
+  }
+
+  const dispatch = action => {
+    state = reducer(state, action);
+    listeners.forEach(listener => listener());
+  };
 
   return {
     dispatch,
-    getState
+    getState,
+    subscribe
   }
 }
--- src/tests/createStore.test.js
+++ src/tests/createStore.test.js
@@ -1,5 +1,49 @@
 import createStore from '../createStore';
 
-test('Create Store', () => {
-  expect(1).toEqual(1);
+describe('Create Store', () => {
+  it('store check', () => {
+    const reducer = $ => $;
+    const { dispatch, getState } = createStore(reducer);
+
+    expect(typeof dispatch).toEqual('function');
+    expect(typeof getState).toEqual('function');
+  });
+
+  it('dispatch', () => {
+    const mockReducer = jest.fn($ => $);
+    const { dispatch } = createStore(mockReducer);
+
+    const action = {type: 'TEST_ACTION'};
+
+    dispatch(action);
+
+    expect(mockReducer.mock.calls.length).toBe(1);
+    expect(mockReducer.mock.calls[0][1]).toBe(action);
+
+  });
+
+  it('subscribe', () => {
+    const reducer = $ => $;
+    const action = {type: 'TEST_ACTION'};
+    const { dispatch, subscribe } = createStore(reducer);
+
+    const mockListener1 = jest.fn($ => $);
+    const mockListener2 = jest.fn($ => $);
+
+    const unsubcribe1 = subscribe(mockListener1);
+    const unsubcribe2 = subscribe(mockListener2);
+
+    dispatch(action);
+
+    expect(mockListener1.mock.calls.length).toBe(1);
+    expect(mockListener2.mock.calls.length).toBe(1);
+    
+    unsubcribe1();
+    unsubcribe2();
+    dispatch(action);
+
+    expect(mockListener1.mock.calls.length).toBe(1);
+    expect(mockListener2.mock.calls.length).toBe(1);
+  });
+
 });
