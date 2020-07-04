4d84ca59efecde0a3d59d227ae55ffa4e22e5f12
Xiaofei ZHANG <xifzhang@thoughtworks.com>
2020-07-04 14:53:55 +0800

    implement combineReducers

---
+++ src/combineReducers.js
@@ -0,0 +1,28 @@
+export default reducersMap => {
+  let nextState = {}, 
+  finalReducer = {};
+
+  const reducerKeys = Object.keys(reducersMap);
+
+  // make sure all reducers are function
+  finalReducer = reducerKeys
+    .filter(key => typeof reducersMap[key] === 'function')
+    .reduce((result, key) => ({ ...result, [key]: reducersMap[key] }), finalReducer);
+
+  // combination
+  return (state, action) => {
+    const finalReducerKeys = Object.keys(finalReducer);
+
+    let hasChanged = false;
+
+    finalReducerKeys.forEach(key => {
+      const currentStateOfKey = state[key];
+      const nextStateOfKey = finalReducer[key](currentStateOfKey, action);
+      nextState[key] = nextStateOfKey;
+
+      hasChanged = currentStateOfKey !== nextStateOfKey;
+    })
+
+    return hasChanged ? nextState : state;
+  }
+};
---
+++ src/tests/combineReducers.test.js
@@ -0,0 +1,79 @@
+import combineReducers from '../combineReducers';
+
+describe('Combine Reducers', () => {
+  it('signle reducer', () => {
+    const userReducer = (state = {}, action) => {
+      if (action.type === 'FETCH_NAME') {
+        return { ...state, name: 'Zeo' };
+      }
+
+      return state;
+    }
+
+    const combination = combineReducers({ user: userReducer });
+    const nextState = combination({}, { type: 'FETCH_NAME' });
+
+    expect(nextState).toEqual({
+      user: { 
+        name: 'Zeo'
+      }
+    });
+  });
+
+  it('multiple reducers', () => {
+    const userReducer = (state = {}, action) => {
+      if (action.type === 'FETCH_NAME') {
+        return { ...state, name: 'Zeo' };
+      }
+
+      return state;
+    }
+
+    const orderReducer = (state = {}, action) => {
+      if (action.type === 'FETCH_ORDER') {
+        return { ...state, price: '$100' };
+      }
+
+      return state;
+    }
+
+    const mockUserReducer = jest.fn(userReducer);
+    const mockOrderReducer = jest.fn(orderReducer);
+
+    const combination = combineReducers({ 
+      user: mockUserReducer,
+      order: mockOrderReducer 
+    });
+
+    const nextState = combination({}, { type: 'FETCH_NAME' });
+
+    expect(nextState).toEqual({
+      user: { 
+        name: 'Zeo'
+      },
+      order: {}
+    });
+    expect(mockUserReducer.mock.calls.length).toBe(1);
+    expect(mockOrderReducer.mock.calls.length).toBe(1);
+
+    const nextNextState = combination(nextState, { type: 'FETCH_ORDER' });
+
+    expect(nextNextState).toEqual({
+      user: { 
+        name: 'Zeo'
+      },
+      order: {
+        price: '$100'
+      }
+    });
+    expect(mockUserReducer.mock.calls.length).toBe(2);
+    expect(mockOrderReducer.mock.calls.length).toBe(2);
+
+    const finalState = combination(nextNextState, { type: 'FETCH_ORDER' });
+
+    expect(finalState).toEqual(nextNextState);
+    expect(mockUserReducer.mock.calls.length).toBe(3);
+    expect(mockOrderReducer.mock.calls.length).toBe(3);
+
+  });
+});
