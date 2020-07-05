080785e42f9656f7c3dc801d6a6811e95e2c1e37
Xiaofei ZHANG <xifzhang@thoughtworks.com>
2020-07-05 13:52:28 +0800

    export dummy-reudx api

--- dummy-redux/index.js
+++ dummy-redux/index.js
@@ -1,1 +1,11 @@
-// export const createStore = (reduces, )
+import createStore from './src/createStore';
+import compose from './src/compose';
+import applyMiddleware from './src/applyMiddleware';
+import combineReducers from './src/combineReducers';
+
+export default {
+  createStore,
+  compose,
+  applyMiddleware,
+  combineReducers
+};
