cffc36b680610d1d946884781637eb248eb25d30
Xiaofei ZHANG <xifzhang@thoughtworks.com>
2020-07-05 23:25:23 +0800

    fix package entry

---
+++ redux/src/index.js
@@ -0,0 +1,11 @@
+import createStore from './createStore';
+import compose from './compose';
+import applyMiddleware from './applyMiddleware';
+import combineReducers from './combineReducers';
+
+export default {
+  createStore,
+  compose,
+  applyMiddleware,
+  combineReducers
+};
