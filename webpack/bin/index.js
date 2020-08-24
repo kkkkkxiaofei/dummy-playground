5abee2411d64c5428e9c6ca8df26e5cca6488ecd
pursue <348564768@qq.com>
2020-08-24 12:53:30 +0800

    rename config -> pack-config

--- webpack/bin/index.js
+++ webpack/bin/index.js
@@ -2,14 +2,14 @@
 
 const argv = require('yargs')
   .usage('Usage: pack [options]')
-  .example('pack --configPath=path/config.js', 'pack your project')
+  .example('pack --configPath=path/pack-config.js', 'pack your project')
   .alias('h', 'help')
   .alias('v', 'version')
   .option('cp', { alias: 'configPath' })
   .argv;
 const fs = require('fs');
 const path = require('path');
-const config = require(argv.configPath ? path.join(process.cwd(), argv.configPath) : `${process.cwd()}/config`);
+const config = require(argv.configPath ? path.join(process.cwd(), argv.configPath) : `${process.cwd()}/pack-config.js`);
 const pack = require('../src/index');
 
 const absDist = `${process.cwd()}/dist`;
--- webpack/examples/vanilla-commonjs/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  entry: './main.js',
-  output: 'chunck.js',
-  library: 'umd-test',
-  libraryTarget: 'umd',
-  presets: [
-    '@babel/preset-env'
-  ]
-}
