48be023cadb22edda340cc0c646e1446a65cebe7
pursue <348564768@qq.com>
2020-09-08 12:19:25 +0800

    add polyfill of object comparasion

--- react-redux/src/utils/shallowEqual.js
+++ react-redux/src/utils/shallowEqual.js
@@ -1,5 +1,15 @@
-export default (A, B) => {
+function is(A, B) {
   if (A === B) {
+    //-Infinite and +Infinite
+    return A !== 0 || B !== 0 || 1/A !== 1/B;
+  } else {
+    //NaN
+    return A !== A && B !== B;
+  }
+}
+
+export default (A, B) => {
+  if (is(A, B)) {
     return true;
   }
 
@@ -14,5 +24,5 @@ export default (A, B) => {
     return false;
   }
 
-  return !keysA.some(keyA => (A[keyA] !== B[keyA]) || !Object.hasOwnProperty.call(B, keyA));
+  return !keysA.some(keyA => !is(A[keyA], B[keyA]) || !Object.hasOwnProperty.call(B, keyA));
 };
