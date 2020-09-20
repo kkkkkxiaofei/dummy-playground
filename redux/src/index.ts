e67822555f3b1e0a8623a1b84a2a6c797a118a34
pursue <348564768@qq.com>
2020-09-20 17:56:24 +0800

    export types

--- redux/src/index.ts
+++ redux/src/index.ts
@@ -2,6 +2,19 @@ import createStore from './createStore';
 import compose from './compose';
 import applyMiddleware from './applyMiddleware';
 import combineReducers from './combineReducers';
+export {
+  State,
+  Action,
+  Listener,
+  Dispatch,
+  StateGetter,
+  Store,
+  Reducer,
+  Enhancer,
+  StoreFactory,
+  ReducerMap,
+  Middleware
+} from './types/index';
 
 export {
   createStore,
