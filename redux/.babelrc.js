631133dfc28648dd4be7528df5bc18e3e6333c73
pursue <348564768@qq.com>
2020-09-20 18:38:48 +0800

    ignore types when using babel

--- redux/.babelrc.js
+++ redux/.babelrc.js
@@ -1,7 +1,8 @@
 const presets = process.env.TYPE === 'es' ?  ['@babel/preset-typescript'] : ['@babel/preset-env', '@babel/preset-typescript']; 
 
 const config = {
-  presets
+  presets,
+  ignore: [/types/]
 }
 
 module.exports = config;
