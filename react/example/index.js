1d24f89f3a4295c4a82520262293e4bb219d487a
pursue <348564768@qq.com>
2020-10-17 23:50:02 +0800

    could render jsx

--- react/example/index.js
+++ react/example/index.js
@@ -1,13 +1,21 @@
 import DummyReact from '../src/react';
+import { render } from '../src/ReactDom'
 
-const Example = ({ title, des, btnName }) => (
+const Example = ({ title, des, btnName, children }) => (
   <div className='container'>
     <h1 className='bold-font'>{title}</h1>
     <p className='content'>{des}</p>
     <div className='button-wrapper'>
       <button disabled={true}>{btnName}</button>
     </div>
+    {children}
   </div>
 )
 
-console.log(<Example title='JSX example' des='jsx' btnName='about me' />)
+render(
+  <Example title='JSX example' des='jsx' btnName='about me'>
+    <div>this is children</div>  
+  </Example>
+  , 
+  document.getElementById('root')
+)
