2b5109c2b6f0109d008ee00d48b76ff06b7cdab8
pursue <348564768@qq.com>
2020-10-29 17:57:45 +0800

    display mock data for profile

--- http/client/components/PostMan.js
+++ http/client/components/PostMan.js
@@ -5,11 +5,28 @@ class PostMan extends Component {
   constructor(props) {
     super(props)
     this.handleClick = this.handleClick.bind(this)
+    this.state = {
+      profile: null
+    }
   }
 
   handleClick() {
     const { method, url, data } = this.props
-    http(method, url, data)
+    http(method, url, data).then(res => this.setState({ profile: res }))
+  }
+
+  renderProfile() {
+    if (this.state.profile) {
+      const { name, email, mobile } = this.state.profile
+      return (
+        <div>
+          <p>{name}</p>
+          <p>{email}</p>
+          <p>{mobile}</p>
+        </div>
+      )
+    }
+    return (<div>no profile</div>)
   }
 
   render() {
@@ -18,6 +35,7 @@ class PostMan extends Component {
       <div>
         <p>{`${method} ${url}`}</p>
         <button onClick={this.handleClick}>send</button>
+        {this.renderProfile()}
       </div>
     )
   }
--- http/server/routers/profile.js
+++ http/server/routers/profile.js
@@ -2,7 +2,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.send({ name: 'test data' })
+  res.send({ 
+    name: 'xzhang',
+    email: 'xzhang@gmail.com',
+    mobile: '167892763446'
+  })
 })
 
 module.exports = router
